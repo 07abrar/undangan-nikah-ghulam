cb917d7c27a1b4ddbde5c47163493bba1b67b261
Ghulam Abrar <gh_abrar51@yahoo.co.id>
2026-04-21 23:41:22 +0900

    Connect RSVP and wishes to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
+import RSVP from "./pages/RSVP"
+import Wishes from "./pages/Wishes"
+
 function App() {
   return (
-    <div>
-      <h1>Ghulam and Yasmin wedding</h1>
-    </div>
+    <>
+      <div>
+        <h1>Ghulam and Yasmin wedding</h1>
+      </div>
+      <div>
+        <nav>
+          <a href="#wishes">Wishes</a>
+          <a href="#rsvp">RSVP</a>
+        </nav>
+        <section id="wishes">
+          <Wishes />
+        </section>
+        <section id="rsvp">
+          <RSVP />
+        </section>
+      </div>
+    </>
   )
 }
 
