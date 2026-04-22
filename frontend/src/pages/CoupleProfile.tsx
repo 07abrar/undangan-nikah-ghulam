d2e12a9ed0639e143df96d401b45d64914a8ce62
Ghulam Abrar <gh_abrar51@yahoo.co.id>
2026-04-23 01:03:48 +0900

    Add temporary couple profile

---
+++ frontend/src/pages/CoupleProfile.tsx
@@ -0,0 +1,32 @@
+export default function CoupleProfile() {
+  return (
+    <div>
+      <h2>Mempelai</h2>
+
+      {/* Bride */}
+      <div id="bride_profile">
+        <img src="" alt="Yasmin Farhana" />
+        <h3>Yasmin Farhana, S.Si.</h3>
+        <p>Putri kedua dari Bapak Prof. Dr. Ir. Husni, M.Agric.Sc (alm)</p>
+        <p>&amp; Ibu Rida Defriana</p>
+        <p>Ibu Yasmin (alm)</p>
+      </div>
+
+      {/* Groom */}
+      <div id="groom_profile">
+        <img src="" alt="Ghulam Abrar" />
+        <h3>Ghulam Abrar, S.T., M.Sc., M.Si.</h3>
+        <p>Putra pertama dari Bapak Chairul, S.E.</p>
+        <p>&amp; Ibu Tihajidah, S.Ag.</p>
+        <a
+          href="https://www.linkedin.com/in/07abrar/"
+          aria-label="LinkedIn profile of Ghulam Abrar"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
+    </div>
+  );
+}
