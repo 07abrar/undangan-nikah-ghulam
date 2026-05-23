a6138d2e8bf5076188865adc76704b59d8b1e958
Ghulam Abrar <gh_abrar51@yahoo.co.id>
2026-05-23 10:21:46 +0900

    Edit tab title and footer text

--- frontend/src/pages/Footer.tsx
+++ frontend/src/pages/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="site-footer">
       <p className="footer-credit">Created by 07abrar</p>
-      <p className="footer-line">Visit my GitHub account</p>
+      <p className="footer-line">Visit my GitHub</p>
       <GithubButton
         href="https://github.com/07abrar"
         label="GitHub profile of Ghulam Abrar"
